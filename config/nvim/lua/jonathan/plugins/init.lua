c2085e08ae6fdfd69aaf4dc31bf0fb9106af3a56
mochele <michelegregorits@gmail.com>
2024-04-28 21:00:52 +0000

    update

--- config/nvim/lua/jonathan/plugins/init.lua
+++ config/nvim/lua/jonathan/plugins/init.lua
@@ -55,6 +55,12 @@ return {
   {
     'ojroques/nvim-osc52',
 		config = function()
+      require('osc52').setup {
+        max_len = 0,
+        silent = false,
+        trim = false,
+        tmux_passthrough = true,
+      }
       vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr = true})
       vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true})
       vim.keymap.set('v', '<leader>c', require('osc52').copy_visual)
