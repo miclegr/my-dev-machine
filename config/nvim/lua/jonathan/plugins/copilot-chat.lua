0012a5a5c73e159036ae234582d3bd950a88b674
michele gregori <michelegregorits@gmail.com>
2025-03-13 16:14:44 +0100

    copilot-on-gemini

--- config/nvim/lua/jonathan/plugins/copilot-chat.lua
+++ config/nvim/lua/jonathan/plugins/copilot-chat.lua
@@ -26,7 +26,85 @@ return {
           end,
         },
       },
-      model = 'claude-3.5-sonnet'
+      providers = {
+        copilot = {
+          disabled = true,
+        },
+        github_models = {
+          disabled = true,
+        },
+        github_embeddings = {
+          disabled = true,
+        },
+        gemini = {
+
+          prepare_input = function(inputs, opts)
+            return require('CopilotChat.config.providers').copilot.prepare_input(inputs, opts)
+          end,
+
+          prepare_output = function(output)
+            return require('CopilotChat.config.providers').copilot.prepare_output(output)
+          end,
+
+          get_headers = function ()
+            local gemini_key = os.getenv("GEMINI_API_KEY")
+            return {
+              ["Authorization"] = 'Bearer ' .. gemini_key,
+            }, nil
+          end,
+
+          get_models = function(headers)
+            local response, err = require('CopilotChat.utils').curl_get('https://generativelanguage.googleapis.com/v1beta/openai/models', {
+                headers = headers,
+                json_response = true,
+            })
+
+            if err then
+                error(err)
+            end
+
+            return vim.tbl_map(function(model)
+                return {
+                    id = model.id,
+                    name = model.id,
+                }
+            end, response.body.data)
+          end,
+
+          embed = function(inputs, headers)
+            local response, err = require('CopilotChat.utils').curl_post('https://generativelanguage.googleapis.com/v1beta/openai/embeddings', {
+                headers = headers,
+                json_request = true,
+                json_response = true,
+                body = {
+                    input = inputs,
+                    model = 'text-embedding-004',
+                },
+            })
+
+            if err then
+                error(err)
+            end
+
+            local data = {}
+            for i, embed in ipairs(response.body.data) do
+              table.insert(data, {
+                    index = i - 1,
+                    embedding = embed.embedding,
+                    object = embed.object,
+                })
+            end
+
+            return data
+
+          end,
+
+          get_url = function()
+            return 'https://generativelanguage.googleapis.com/v1beta/openai/chat/completions'
+          end,
+          },
+      },
+      model = 'models/gemini-1.5-pro',
     },
   },
 }
